d563ba7b2bba276255fbec8acbfd67dbd4706c3e
MikhailShevchuk <cp91@mail.ru>
2021-10-22 17:02:11 +0500

    Add drag begin script

---
+++ Roll & Don't Fall/Assets/Scripts/Common/DragBegin.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class DragBegin : MonoBehaviour, IBeginDragHandler
+{
+    [SerializeField]
+    private UnityEvent _dragBeginning;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        _dragBeginning?.Invoke();
+        Debug.Log(eventData.position);
+    }
+}
