5c8979dfe29b93d6e64b94ede46fcb839b356d8f
MikhailShevchuk <cp91@mail.ru>
2021-10-23 14:55:15 +0500

    Add ellipse checker

---
+++ Roll & Don't Fall/Assets/Scripts/Common/EllipseInsideEllipseChecker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EllipseInsideEllipseChecker : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform _smallEllipse;
+
+    [SerializeField]
+    private RectTransform _bigEllipse;
+
+    public bool CheckEllipseInside()
+    {
+        float r = _smallEllipse.rect.width / 2;
+        float R = _bigEllipse.rect.width / 2;
+        if (R * R > Mathf.Pow(_bigEllipse.rect.position.x - _smallEllipse.rect.position.x, 2) +
+            Mathf.Pow(_bigEllipse.rect.position.y - _smallEllipse.rect.position.y, 2) +
+            r * r)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
