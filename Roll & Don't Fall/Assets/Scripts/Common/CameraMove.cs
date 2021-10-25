67acca0c6c66880430ae8bdba5d9e1e486465c1d
MikhailShevchuk <cp91@mail.ru>
2021-10-25 19:34:24 +0500

    Add camera mover

---
+++ Roll & Don't Fall/Assets/Scripts/Common/CameraMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _character;
+
+    [SerializeField]
+    private float _x_pos;
+
+    [SerializeField]
+    private float _y_pos;
+
+    [SerializeField]
+    private float _z_pos;
+
+    void Update()
+    {
+        Vector3 pos = _character.transform.position;
+        this.transform.position = new Vector3(pos.x + _x_pos, pos.y + _y_pos, pos.z + _z_pos);
+    }
+}
--- Roll & Don't Fall/Assets/Scripts/Common/EllipseInsideEllipseChecker.cs
+++ Roll & Don't Fall/Assets/Scripts/Common/EllipseInsideEllipseChecker.cs
@@ -13,8 +13,8 @@ public class EllipseInsideEllipseChecker : MonoBehaviour
         float r = _smallEllipse.rect.width / 2;
         float R = _bigEllipse.rect.width / 2;
         if (R * R > Mathf.Pow(_bigEllipse.position.x - newPosition.x, 2) +
-            Mathf.Pow(_bigEllipse.position.y - newPosition.y, 2) +
-            r * r)
+            Mathf.Pow(_bigEllipse.position.y - newPosition.y, 2)
+            + 5 * r * r)
         {            
             return true;            
         }
