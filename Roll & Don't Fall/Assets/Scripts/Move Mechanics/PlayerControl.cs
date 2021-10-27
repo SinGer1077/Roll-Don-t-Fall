3ba80373a04865997dabdb90d8123489c259e0f1
MikhailShevchuk <cp91@mail.ru>
2021-10-27 11:42:28 +0500

    Add test player control

---
+++ Roll & Don't Fall/Assets/Scripts/Move Mechanics/PlayerControl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerInput _playerInput;
+
+    private void Update()
+    {
+        if (_playerInput.actions["Move"].triggered)
+        {
+            Vector2 value = _playerInput.actions["Move"].ReadValue<Vector2>();
+            Debug.Log(value);
+        }
+    }
+}
