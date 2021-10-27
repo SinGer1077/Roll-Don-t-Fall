923c8062d529cf92420842a2af740deb49cf334d
MikhailShevchuk <cp91@mail.ru>
2021-10-27 16:32:11 +0500

    All works on horizontal area, maybe need to fix inertion speed

--- Roll & Don't Fall/Assets/Scripts/Move Mechanics/SmoothStop.cs
+++ Roll & Don't Fall/Assets/Scripts/Move Mechanics/SmoothStop.cs
@@ -7,27 +7,20 @@ public class SmoothStop : MonoBehaviour
 
     private float _speed = 1.0f;
 
-    private float _startTime;
-
-    private float _journeyLength;
-
     private Vector3 _startMarker;
 
     private Vector3 _velocity;
 
     private void Start()
-    {
-        _startTime = Time.time;
-
+    {        
         _velocity = Vector3.zero;
 
-        _startMarker = new Vector3(0, _body.velocity.y, 0);
-
-        _journeyLength = Vector3.Distance(_startMarker, _body.velocity);
+        _startMarker = new Vector3(0, _body.velocity.y, 0);        
     }
 
     public void SmoothStopping()
     {
+        _speed = Mathf.Max(Mathf.Abs(_body.velocity.x), Mathf.Abs(_body.velocity.z));
         _body.velocity = Vector3.SmoothDamp(_body.velocity, _startMarker, ref _velocity, _speed);
     }
 }
