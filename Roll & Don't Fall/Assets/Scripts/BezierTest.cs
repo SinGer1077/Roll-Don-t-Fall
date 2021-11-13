e4b510986c76d830d5ec3302fe867f753f1c4d21
MikhailShevchuk <cp91@mail.ru>
2021-11-13 17:14:41 +0500

    Add bezier point calculation and bezier test movement

---
+++ Roll & Don't Fall/Assets/Scripts/BezierTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class BezierTest : MonoBehaviour
+{
+    [SerializeField]
+    private Transform p0;
+
+    [SerializeField]
+    private Transform p1;
+
+    [SerializeField]
+    private Transform p2;
+
+    [SerializeField]
+    private Transform p3;
+
+    [SerializeField, Range(0, 1)]
+    private float time;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Bezier.GetPoint(p0.position, p1.position, p2.position, p3.position, time);
+    }
+}
