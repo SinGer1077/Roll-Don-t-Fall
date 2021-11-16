46bf6202d40d91698e17fbb474a581941c2cba46
MikhailShevchuk <cp91@mail.ru>
2021-11-17 00:03:28 +0500

    New scripts

---
+++ Roll & Don't Fall/Assets/Scripts/TrackChank.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackChank
+{
+    private Vector3[] _basePoints;
+
+    public Vector3[] BasePoints => _basePoints;
+
+    private int _sigmentNumbers = 12;
+
+    public void FormChank()
+    {
+        List<Vector3> vertices = new List<Vector3>();
+        List<int> triangles = new List<int>();
+        List<Vector2> uv = new List<Vector2>();
+
+        Vector3 firstPrevPoint = _basePoints[0];
+        Vector3 secondPrevPoint = new Vector3(_basePoints[0].x + 5, _basePoints[0].y, _basePoints[0].z);
+    }
+}
---
+++ Roll & Don't Fall/Assets/Scripts/TrackChankGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackChankGenerator : MonoBehaviour
+{
+
+
+    /// <summary>
+    /// список сгенерированных чанков
+    /// </summary>
+    private List<GameObject> _chankList;
+
+    private void Start()
+    {
+        _chankList = new List<GameObject>();
+
+
+    }
+
+    private void GenerateChank(int chankCount)
+    {
+
+    }
+}
