581b3e5f6499f88c1eede318be9d8800645d60a6
MikhailShevchuk <cp91@mail.ru>
2021-11-18 19:05:20 +0500

    Generating track mesh

--- Roll & Don't Fall/Assets/Scripts/TrackChank.cs
+++ Roll & Don't Fall/Assets/Scripts/TrackChank.cs
@@ -8,7 +8,20 @@ public class TrackChank
 
     public Vector3[] BasePoints => _basePoints;
 
-    private int _sigmentNumbers = 12;
+    private int _sigmentNumber = 12;
+
+    private Vector3 _axisDistance = new Vector3(5f, 0, 0);
+
+    public Vector3 AxisDistance => _axisDistance;
+
+    private Mesh _mesh;
+
+    public Mesh ChankMesh => _mesh;
+
+    public TrackChank(Vector3[] points)
+    {
+        _basePoints = points;
+    }
 
     public void FormChank()
     {
@@ -16,7 +29,45 @@ public class TrackChank
         List<int> triangles = new List<int>();
         List<Vector2> uv = new List<Vector2>();
 
-        Vector3 firstPrevPoint = _basePoints[0];
-        Vector3 secondPrevPoint = new Vector3(_basePoints[0].x + 5, _basePoints[0].y, _basePoints[0].z);
+        //Vector3 firstPrevPoint = _basePoints[0];
+        //Vector3 secondPrevPoint = new Vector3(_basePoints[0].x + _axisDistance.x, _basePoints[0].y + _axisDistance.y, _basePoints[0].z + _axisDistance.z);
+
+        for (int i = 0; i < _sigmentNumber; i++)
+        {
+            float parameter = (float)i / _sigmentNumber;
+            Vector3 point = Bezier.GetPoint(_basePoints[0], _basePoints[1], _basePoints[2], _basePoints[3], parameter);
+            Vector3 secondPoint = new Vector3(point.x + _axisDistance.x, point.y + _axisDistance.y, point.z + _axisDistance.z);
+
+            vertices.Add(secondPoint);
+            vertices.Add(point);
+            uv.Add(point);
+            uv.Add(secondPoint);
+
+            //firstPrevPoint = point;
+            //secondPrevPoint = secondPoint;
+        }
+
+        for (int i = 0; i < _sigmentNumber * 2 - 2; i += 2)
+        {
+            triangles.Add(i);
+            triangles.Add(i + 1);
+            triangles.Add(i + 2);
+
+            triangles.Add(i + 3);
+            triangles.Add(i + 2);
+            triangles.Add(i + 1);
+        }
+
+        _mesh = new Mesh();
+
+        _mesh.vertices = vertices.ToArray();
+        _mesh.uv = uv.ToArray();
+        _mesh.triangles = triangles.ToArray();      
+        _mesh.RecalculateNormals();
+    }    
+
+    public void SetDistance(Vector3 newDistance)
+    {
+        _axisDistance = newDistance;
     }
 }
--- Roll & Don't Fall/Assets/Scripts/TrackChankGenerator.cs
+++ Roll & Don't Fall/Assets/Scripts/TrackChankGenerator.cs
@@ -4,22 +4,132 @@ using UnityEngine;
 
 public class TrackChankGenerator : MonoBehaviour
 {
+    [SerializeField]
+    private Transform _parent;
 
+    [SerializeField]
+    private Transform _firstPosition;
 
-    /// <summary>
-    /// список сгенерированных чанков
-    /// </summary>
+    [SerializeField]
+    private int _defaultChanksCount = 10;
+
     private List<GameObject> _chankList;
+
+    private List<TrackChank> _chankData;
+
+    private Vector3 _lastPointPosition;
 
     private void Start()
     {
+        Debug.Log("Программа началась ");
+
         _chankList = new List<GameObject>();
+        _chankData = new List<TrackChank>();
+        _lastPointPosition = _firstPosition.position;
 
+        GenerateChanks(_defaultChanksCount);
 
+        //Example();
     }
 
-    private void GenerateChank(int chankCount)
+    private void GenerateChanks(int chankCount)
     {
+        for (int i = 0; i < chankCount; i++)
+        {
+            GameObject chank = new GameObject("Chank");            
+            chank.transform.SetParent(_parent);
 
+            TrackChank chankData = new TrackChank(new Vector3[] {_lastPointPosition,  NextPositionRandomizer(), NextPositionRandomizer(), NextPositionRandomizer()});
+            chankData.FormChank();
+
+            MeshRenderer meshRenderer = chank.AddComponent<MeshRenderer>();
+            meshRenderer.sharedMaterial = new Material(Shader.Find("Standard"));
+
+            MeshFilter meshFilter = chank.AddComponent<MeshFilter>();            
+            meshFilter.sharedMesh =  chankData.ChankMesh;            
+
+            MeshCollider collider = chank.AddComponent<MeshCollider>();
+            collider.sharedMesh = chankData.ChankMesh;
+
+            //Quaternion 
+            //chank.transform.rotation
+
+            _chankList.Add(chank);
+            _chankData.Add(chankData);           
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        //for (int i = 0; i < _chankData[0].ChankMesh.vertices.Length; i++)
+        //{
+        //    Gizmos.DrawSphere(_chankData[0].ChankMesh.vertices[i], 0.1f);
+        //}
+
+        //foreach (TrackChank x in _chankData)
+        //{
+        //    Gizmos.DrawMesh(x.ChankMesh);
+        //}
+    }
+
+    private Vector3 NextPositionRandomizer()
+    {
+        float z = Random.Range(5f, 15f);
+        float x = Random.Range(-15f, 15f);
+        Vector3 result = new Vector3(_lastPointPosition.x + x, _lastPointPosition.y, _lastPointPosition.z + z);
+        _lastPointPosition = result;
+        
+        return _lastPointPosition;
+    }
+
+    public void Example()
+    {
+        float width = 1;
+        float height = 1;
+
+        MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
+        meshRenderer.sharedMaterial = new Material(Shader.Find("Standard"));
+
+        MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
+
+        Mesh mesh = new Mesh();
+
+        Vector3[] vertices = new Vector3[4]
+        {
+            new Vector3(0, 0, 0),
+            new Vector3(width, 0, 0),
+            new Vector3(0, height, 0),
+            new Vector3(width, height, 0)
+        };
+        mesh.vertices = vertices;
+
+        int[] tris = new int[6]
+        {
+            // lower left triangle
+            0, 2, 1,
+            // upper right triangle
+            2, 3, 1
+        };
+        mesh.triangles = tris;
+
+        Vector3[] normals = new Vector3[4]
+        {
+            -Vector3.forward,
+            -Vector3.forward,
+            -Vector3.forward,
+            -Vector3.forward
+        };
+        //mesh.normals = normals;
+
+        Vector2[] uv = new Vector2[4]
+        {
+            new Vector2(0, 0),
+            new Vector2(1, 0),
+            new Vector2(0, 1),
+            new Vector2(1, 1)
+        };
+        //mesh.uv = uv;
+
+        meshFilter.mesh = mesh;
     }
 }
