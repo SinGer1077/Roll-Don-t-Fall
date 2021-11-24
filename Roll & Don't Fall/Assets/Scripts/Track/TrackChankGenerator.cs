a9c876544ba26ac384c8f0b1023835dde4d233f4
MikhailShevchuk <cp91@mail.ru>
2021-11-24 14:23:58 +0500

    Add namespaces

---
+++ Roll & Don't Fall/Assets/Scripts/Track/TrackChankGenerator.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace RollDontFall.TrackModule
+{
+    public class TrackChankGenerator : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform _parent;
+
+        [SerializeField]
+        private Transform _firstPosition;
+
+        [SerializeField]
+        private int _defaultChanksCount = 100;
+
+        private List<GameObject> _chankList;
+
+        private List<TrackChank> _chankData;
+
+        private Vector3 _lastPointPosition;
+
+        private float _trackFirstPointX = 0f;
+
+        private float _trackFirstPointZ = 0f;
+
+        private float _trackSecondPointX = 0f;
+
+        private float _trackSecondPointZ = 0f;
+
+        private void Start()
+        {
+            Debug.Log("Программа началась ");
+
+            _chankList = new List<GameObject>();
+            _chankData = new List<TrackChank>();
+            _lastPointPosition = _firstPosition.position;
+
+            GenerateChanks(_defaultChanksCount);
+        }
+
+        private void GenerateChanks(int chankCount)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                GameObject chank = new GameObject("Chank");
+                chank.transform.SetParent(_parent);
+
+                //TrackChank chankData = new TrackChank(new Vector3[] {_lastPointPosition,  NextFirstpointPositionRandomizer(), NextSecondpointPositionRandomizer(), NextFirstpointPositionRandomizer()});
+                int random = Random.Range(0, 3);
+                TrackChank chankData = new TrackChank(TrackTypes.GetRandomTrackType(_lastPointPosition, random));
+                chankData.FormChank();
+
+                MeshRenderer meshRenderer = chank.AddComponent<MeshRenderer>();
+                meshRenderer.sharedMaterial = new Material(Shader.Find("Standard"));
+
+                MeshFilter meshFilter = chank.AddComponent<MeshFilter>();
+                meshFilter.sharedMesh = chankData.ChankMesh;
+
+                MeshCollider collider = chank.AddComponent<MeshCollider>();
+                collider.sharedMesh = chankData.ChankMesh;
+
+                _chankList.Add(chank);
+                _chankData.Add(chankData);
+
+                _lastPointPosition = chankData.BasePoints[3];
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+
+        }
+
+        private Vector3 NextFirstpointPositionRandomizer()
+        {
+            _trackFirstPointX += 30f;
+            _trackFirstPointZ += 10f;
+            Vector3 result = new Vector3(_lastPointPosition.x + _trackFirstPointX, _lastPointPosition.y, _lastPointPosition.z + _trackFirstPointZ);
+            _lastPointPosition = result;
+            Debug.Log(_lastPointPosition);
+
+            return _lastPointPosition;
+        }
+        private Vector3 NextSecondpointPositionRandomizer()
+        {
+            _trackSecondPointX += -30f;
+            _trackSecondPointZ += 10f;
+            Vector3 result = new Vector3(_lastPointPosition.x + _trackSecondPointX, _lastPointPosition.y, _lastPointPosition.z + _trackSecondPointZ);
+            _lastPointPosition = result;
+            Debug.Log(_lastPointPosition);
+
+            return _lastPointPosition;
+        }
+    }
+}
---
+++ Roll & Don't Fall/Assets/Scripts/Track/TrackTypes.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+namespace RollDontFall.TrackModule
+{
+    public static class TrackTypes
+    {
+        private static float trackLength = 40f;
+
+        public static Vector3[] Straight(Vector3 firstPoint)
+        {
+            Vector3 secondPosition = new Vector3(firstPoint.x, firstPoint.y, firstPoint.z + trackLength / 4);
+            Vector3 thirdPosition = new Vector3(firstPoint.x, firstPoint.y, secondPosition.z + trackLength / 4);
+            Vector3 fourthPosition = new Vector3(firstPoint.x, firstPoint.y, thirdPosition.z + trackLength / 4);
+            return new Vector3[] { firstPoint, secondPosition, thirdPosition, fourthPosition };
+        }
+
+        public static Vector3[] HorizontalArc(Vector3 firstPoint)
+        {
+            float arcDistance = Random.Range(-100f, 100f);
+
+            Vector3 secondPosition = new Vector3(firstPoint.x, firstPoint.y, firstPoint.z + trackLength / 4);
+            Vector3 thirdPosition = new Vector3(firstPoint.x + arcDistance, firstPoint.y, secondPosition.z + trackLength / 4);
+            Vector3 fourthPosition = new Vector3(firstPoint.x, firstPoint.y, thirdPosition.z + trackLength / 4);
+            return new Vector3[] { firstPoint, secondPosition, thirdPosition, fourthPosition };
+        }
+
+        public static Vector3[] DoubleHorizontalArc(Vector3 firstPoint)
+        {
+            float firstArcDistance = Random.Range(-100f, 100f);
+            float secondArcDistance = -firstArcDistance;
+
+            Vector3 secondPosition = new Vector3(firstPoint.x + firstArcDistance, firstPoint.y, firstPoint.z + trackLength / 4);
+            Vector3 thirdPosition = new Vector3(firstPoint.x + secondArcDistance, firstPoint.y, secondPosition.z + trackLength / 4);
+            Vector3 fourthPosition = new Vector3(firstPoint.x, firstPoint.y, thirdPosition.z + trackLength / 4);
+            return new Vector3[] { firstPoint, secondPosition, thirdPosition, fourthPosition };
+        }
+
+        public static Vector3[] GetRandomTrackType(Vector3 firstPoint, int trackType)
+        {
+            Vector3[] array = new Vector3[] { };
+            switch (trackType)
+            {
+                case 0:
+                    array = Straight(firstPoint);
+                    break;
+                case 1:
+                    array = HorizontalArc(firstPoint);
+                    break;
+                case 2:
+                    array = DoubleHorizontalArc(firstPoint);
+                    break;
+            }
+            return array;
+        }
+    }
+}
