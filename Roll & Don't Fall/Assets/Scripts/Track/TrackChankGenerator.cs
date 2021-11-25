53528e72253eec60851f56d890d3f6ecd297db2d
MikhailShevchuk <cp91@mail.ru>
2021-11-25 15:23:06 +0500

    Add animators

--- Roll & Don't Fall/Assets/Scripts/Track/TrackChankGenerator.cs
+++ Roll & Don't Fall/Assets/Scripts/Track/TrackChankGenerator.cs
@@ -12,21 +12,16 @@ namespace RollDontFall.TrackModule
         private Transform _firstPosition;
 
         [SerializeField]
-        private int _defaultChanksCount = 100;
+        private int _defaultChanksCount = 10;
+
+        [SerializeField]
+        private RuntimeAnimatorController _chankUpDownAnimation;
 
         private List<GameObject> _chankList;
 
         private List<TrackChank> _chankData;
 
         private Vector3 _lastPointPosition;
-
-        private float _trackFirstPointX = 0f;
-
-        private float _trackFirstPointZ = 0f;
-
-        private float _trackSecondPointX = 0f;
-
-        private float _trackSecondPointZ = 0f;
 
         private void Start()
         {
@@ -41,12 +36,11 @@ namespace RollDontFall.TrackModule
 
         private void GenerateChanks(int chankCount)
         {
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < chankCount; i++)
             {
                 GameObject chank = new GameObject("Chank");
                 chank.transform.SetParent(_parent);
 
-                //TrackChank chankData = new TrackChank(new Vector3[] {_lastPointPosition,  NextFirstpointPositionRandomizer(), NextSecondpointPositionRandomizer(), NextFirstpointPositionRandomizer()});
                 int random = Random.Range(0, 3);
                 TrackChank chankData = new TrackChank(TrackTypes.GetRandomTrackType(_lastPointPosition, random));
                 chankData.FormChank();
@@ -62,13 +56,11 @@ namespace RollDontFall.TrackModule
 
                 if (random == (int)TrackTypes.Types.Straight)
                 {
-                    if (chankData.GetChankLength() <= 10f)
+                    int changeToAnimate = Random.Range(0, 100);
+                    if (changeToAnimate > 75)
                     {
-                        chank.AddComponent<ChankUpDown>();
-                    }
-                    else
-                    {
-                        chank.AddComponent<ChankDynamicScale>();
+                        Animator animator = chank.AddComponent<Animator>();
+                        animator.runtimeAnimatorController = _chankUpDownAnimation;
                     }
                 }
 
@@ -77,32 +69,6 @@ namespace RollDontFall.TrackModule
 
                 _lastPointPosition = chankData.BasePoints[3];
             }
-        }
-
-        private void OnDrawGizmos()
-        {
-
-        }
-
-        private Vector3 NextFirstpointPositionRandomizer()
-        {
-            _trackFirstPointX += 30f;
-            _trackFirstPointZ += 10f;
-            Vector3 result = new Vector3(_lastPointPosition.x + _trackFirstPointX, _lastPointPosition.y, _lastPointPosition.z + _trackFirstPointZ);
-            _lastPointPosition = result;
-            Debug.Log(_lastPointPosition);
-
-            return _lastPointPosition;
-        }
-        private Vector3 NextSecondpointPositionRandomizer()
-        {
-            _trackSecondPointX += -30f;
-            _trackSecondPointZ += 10f;
-            Vector3 result = new Vector3(_lastPointPosition.x + _trackSecondPointX, _lastPointPosition.y, _lastPointPosition.z + _trackSecondPointZ);
-            _lastPointPosition = result;
-            Debug.Log(_lastPointPosition);
-
-            return _lastPointPosition;
-        }
+        }        
     }
 }
