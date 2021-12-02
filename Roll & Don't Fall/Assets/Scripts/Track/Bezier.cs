ed1860116cec8f563fdef9e16b877659dc7b2ad1
MikhailShevchuk <cp91@mail.ru>
2021-12-02 14:54:44 +0500

    Add get first derivative

--- Roll & Don't Fall/Assets/Scripts/Track/Bezier.cs
+++ Roll & Don't Fall/Assets/Scripts/Track/Bezier.cs
@@ -28,5 +28,15 @@ namespace RollDontFall.TrackModule
                 3f * oneMinusT * t * t * p2 +
                 t * t * t * p3;
         }
+
+        public static Vector3 GetFirstDerivetive(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
+        {
+            t = Mathf.Clamp01(t);
+            float oneMinusT = 1f - t;
+            return
+                3f * oneMinusT * oneMinusT * (p1 - p0) +
+                6f * oneMinusT * t * (p2 - p1) +
+                3f * t * t * (p3 - p2);
+        }
     }
 }
